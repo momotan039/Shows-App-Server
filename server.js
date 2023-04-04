d4b382ea51477a891fa5c9e4dc4fa0b32bf55141
MOHAMMED <momotaha039@gmail.com>
2023-04-05 00:22:42 +0300

    add dotenv and db connection

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+require('dotenv').config();
 const express = require("express");
+const { connect } = require('./services/database');
 const server = express();
+//connect to database
+connect()
+//add midleware
+server.use(express.json())
 
+//add routes
+server.use(require('./controllers/auth/login'))
+server.use(require('./controllers/users'))
 
 server.listen(5000, () => {
   console.log("server start up");
