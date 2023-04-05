c6124f1b27e0bf68e120fcf5dc6e0d2ea80d50dc
momotan <momotaha039@gmail.com>
2023-04-05 11:19:35 +0300

    update database

--- services/database.js
+++ services/database.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const uri = 'mongodb://localhost:27017/app1'; // replace with your own database URI
+const uri = 'mongodb://localhost:27017/app1';
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
