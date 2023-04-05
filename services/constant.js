d78c9307263769b5c3fd5c346ef4fd25cf045981
momotan <momotaha039@gmail.com>
2023-04-05 19:47:13 +0300

    edit constant file

--- services/constant.js
+++ services/constant.js
@@ -1,3 +1,6 @@
 const Api=process.env.API
 const jwt=require('jsonwebtoken')
-module.exports={Api,jwt}
+const KeyApi=process.env.KEY
+const Shows_API=process.env.SHOWS_API
+const Error_Message={message:'occured error While handel the process'}
+module.exports={Api,jwt,KeyApi,Shows_API,Error_Message}
