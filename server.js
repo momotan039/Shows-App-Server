f02a86c24735048f9463cb14e03855bac2bb6a49
momotan <momotaha039@gmail.com>
2023-04-05 19:45:51 +0300

    add new route

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const authorization = require('./middleware/authorization');
 const { Api } = require('./services/constant');
 const { connect } = require('./services/database');
 const cookieParser = require("cookie-parser");
+const { getAllGenres } = require('./services/showsAPI');
 //connect to database
 connect()
 //add midleware
@@ -14,7 +15,8 @@ server.use(express.json())
 //add routes
 server.use(require('./controllers/auth/login'))
 server.use(require('./controllers/auth/register'))
-server.use(require('./controllers/users'))
+server.use(require('./controllers/user/users'))
+server.use(require('./controllers/user/preferences'))
 
 server.listen(5000, () => {
   console.log("server start up");
