9d79eec03af7cea6308608637c3f76b7d572153c
momotan <momotaha039@gmail.com>
2023-04-05 12:55:58 +0300

    edit server file

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 require('dotenv').config();
 const express = require("express");
-const { connect } = require('./services/database');
 const server = express();
+const authorization = require('./middleware/authorization');
+const { Api } = require('./services/constant');
+const { connect } = require('./services/database');
+const cookieParser = require("cookie-parser");
 //connect to database
 connect()
 //add midleware
+server.use(cookieParser())
+server.use(Api,(req,res,next)=>authorization(req,res,next))
 server.use(express.json())
-
 //add routes
 server.use(require('./controllers/auth/login'))
+server.use(require('./controllers/auth/register'))
 server.use(require('./controllers/users'))
 
 server.listen(5000, () => {
