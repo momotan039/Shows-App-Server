b235fa9955beffa5ee65931cf0007bbf55512005
momotan <momotaha039@gmail.com>
2023-04-05 20:35:37 +0300

    about to finish preference

--- controllers/user/preferences.js
+++ controllers/user/preferences.js
@@ -1,17 +1,26 @@
-const express=require('express')
-const { ObjectId } = require('mongodb')
-const { Api, Error_Message } = require('../../services/constant')
-const { db } = require('../../services/database')
-const router=express.Router()
-router.post(Api+'/setup-account',async (req,res)=>{
-    const {lang,show_lang,genres}=req.body
-   try {
-    await db.collection('users').updateOne({_id:new ObjectId(req.user.id)},{
-        $set:{...req.body}
-    })
-   } catch (error) {
-    res.send(Error_Message)
-   }
-})
+const express = require("express");
+const { ObjectId } = require("mongodb");
+const { Api, Error_Message } = require("../../services/constant");
+const { db } = require("../../services/database");
+const router = express.Router();
+router.post(Api + "/setup-account", async (req, res) => {
+  const { lang, show_lang, genres } = req.body;
+  try {
+    const f='642d7ff2af1a56c5cd55a0e4'
+    const result = await db.collection("users").findOneAndUpdate(
+      { _id: new ObjectId(f) },
+      {
+        $set:req.body,
+      },
+      {
+        returnDocument:true
+      }
+    );
+    const {password,...user}=result.value
+    res.send(user)
+  } catch (error) {
+    res.send(Error_Message);
+  }
+});
 
-module.exports=router
+module.exports = router;
