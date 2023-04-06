b906cfc2e32d749ba1e73c88431e6d467d91d1ab
momotan <momotaha039@gmail.com>
2023-04-06 19:54:32 +0300

    edit server file

--- server.js
+++ server.js
@@ -5,18 +5,20 @@ const authorization = require('./middleware/authorization');
 const { Api } = require('./services/constant');
 const { connect } = require('./services/database');
 const cookieParser = require("cookie-parser");
-const { getAllGenres } = require('./services/showsAPI');
+const { getAllGenres } = require('./services/shows api/showsAPI');
 //connect to database
 connect()
 //add midleware
 server.use(cookieParser())
 server.use(Api,(req,res,next)=>authorization(req,res,next))
 server.use(express.json())
+server.use(express.query())
 //add routes
 server.use(require('./controllers/auth/login'))
 server.use(require('./controllers/auth/register'))
 server.use(require('./controllers/user/users'))
-server.use(require('./controllers/user/preferences'))
+server.use(require('./controllers/user/setupAccount'))
+server.use(require('./controllers/shows/shows'))
 
 server.listen(5000, () => {
   console.log("server start up");
