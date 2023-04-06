95e0109cd463e05e6ad6607fdec9f241cf991310
momotan <momotaha039@gmail.com>
2023-04-06 19:52:02 +0300

    add moives api service

---
+++ services/shows api/movies.js
@@ -0,0 +1,32 @@
+const { default: axios } = require("axios")
+const { Shows_API, KeyApi } = require("../constant")
+
+
+const getMovies=async(query)=>{
+    const res=await axios.get(`${Shows_API}/discover/movie`,{
+      params:{
+        with_cast:query.cast,
+        with_genres:query.genres,
+        with_original_language:query.show_lang,
+        api_key:KeyApi
+      }
+    })
+    return res.data
+}
+
+const getMyMovies=async(user)=>{
+  const {lang,show_lang,genres}=user.preferences
+  const res=await axios.get(`${Shows_API}/discover/movie`,{
+    params:{
+      language:lang,
+      with_genres:genres.map(g=>g.id).join(','),
+      with_original_language:show_lang,
+      api_key:KeyApi
+    }
+  })
+  return res.data
+}
+
+
+module.exports={getMovies,getMyMovies}
+
