400910df70b5345d379acccd238335e91f51e84d
momotan <momotaha039@gmail.com>
2023-04-06 19:53:23 +0300

    edit authorize file

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,18 +1,28 @@
+const { ObjectId } = require("mongodb");
 const { jwt } = require("../services/constant");
+const { db } = require("../services/database");
 function authorization(req, res, next) {
   const token =  req.cookies.token;
+  //check if is token
     if (!token) {
       res.status(401).json({ message: 'Unauthorized' });
       return;
     }
-   
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, user) => {
       if (err) {
         res.status(401).json({ message: 'Session expired... Please login again' });
         return;
       }
   
-      req.user = decoded;
+      //check if current user exist
+      const u=await db.collection('users').findOne({_id:new ObjectId(user.id)})
+      if(!u)
+      {
+        res.status(401).json({ message: 'Unauthorized' });
+        return
+      }
+      const {password,..._user}=u
+      req.user = _user;
       next();
     });
   }
