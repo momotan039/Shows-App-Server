97ffe201d6021ff59eea67b3f87d3b122d789a1c
momotan <momotaha039@gmail.com>
2023-04-06 19:54:08 +0300

    add setup account route

---
+++ controllers/user/setupAccount.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { ObjectId } = require("mongodb");
+const { Api, Error_Message } = require("../../services/constant");
+const { db } = require("../../services/database");
+const router = express.Router();
+router.post(Api + "/setup-account", async (req, res) => {
+  const preferences={'preferences':req.body}
+  try {
+    const result = await db.collection("users").findOneAndUpdate(
+      { _id: new ObjectId(req.user._id) },
+      {
+        $set:preferences,
+      },
+      {
+        returnDocument:true
+      }
+    );
+    res.send({...result.value,...preferences})
+  } catch (error) {
+    res.send(error+'');
+  }
+});
+
+module.exports = router;
