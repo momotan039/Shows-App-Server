bf682ff3f4cb667d75107c6d12f3760f380bf45b
momotan <momotaha039@gmail.com>
2023-04-09 17:38:59 +0300

    add genres route

--- controllers/shows/shows.js
+++ controllers/shows/shows.js
@@ -3,46 +3,59 @@ const { ObjectId } = require("mongodb");
 const { Api, Error_Message } = require("../../services/constant");
 const { db } = require("../../services/database");
 const { getMovies, getMyMovies } = require("../../services/shows api/movies");
-const { getTrend, getShowById } = require("../../services/shows api/showsAPI");
+const {
+  getTrend,
+  getShowById,
+  getAllGenres,
+} = require("../../services/shows api/showsAPI");
 const { getTvShows, getMyTvShows } = require("../../services/shows api/tv");
 const router = express.Router();
 
-
 router.get(Api + "/shows/trending", async (req, res) => {
-    const shows = await getTrend("all", "day");
-    res.send(shows);
+  const shows = await getTrend("all", "day");
+  res.send(shows);
 });
 
 router.get(Api + "/shows/:type", async (req, res) => {
-    const { type } = req.params;
-    let shows;
-    if (type === "movies") shows = await getMovies(req.query);
-    else if (type === "tv") shows = await getTvShows(req.query);
-    else {
-        res.send("invalid type show!!");
-        return;
-    }
-    res.send(shows);
+  const { type } = req.params;
+  let shows;
+  if (type === "movies") shows = await getMovies(req.query);
+  else if (type === "tv") shows = await getTvShows(req.query);
+  else {
+    res.send("invalid type show!!");
+    return;
+  }
+  res.send(shows);
 });
 
 router.get(Api + "/shows/:type/recommended", async (req, res) => {
-    const { type } = req.params;
-    let shows;
-    if (type === "movies") shows = await getMyMovies(req.user);
-    else if (type === "tv") shows = await getMyTvShows(req.user);
-    else {
-        res.send("invalid type show!!");
-        return;
-    }
+  const { type } = req.params;
+  let shows;
+  if (type === "movies") shows = await getMyMovies(req.user);
+  else if (type === "tv") shows = await getMyTvShows(req.user);
+  else {
+    res.send("invalid type show!!");
+    return;
+  }
   res.send(shows);
 });
 
 router.get(Api + "/show/:type/:show_id", async (req, res) => {
   try {
     const { type, show_id } = req.params;
-    const show = await getShowById(type, show_id,req.user.preferences.lang);
+    const show = await getShowById(type, show_id, req.user.preferences.lang);
     res.send(show);
-  } catch (error) {res.send(Error_Message)}
+  } catch (error) {
+    res.send(Error_Message);
+  }
+});
+
+router.get(Api + "/genres", async(req, res) => {
+  await getAllGenres()
+    .then((data) =>{
+      res.send(data)
+    })
+    .catch((err) => res.send(err));
 });
 
 module.exports = router;
