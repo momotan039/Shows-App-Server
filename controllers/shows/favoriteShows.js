3c57d5ac68f1324d3ef459a52be820ab0a0696e8
momotan <momotaha039@gmail.com>
2023-04-12 21:05:06 +0300

    add remove function

--- controllers/shows/favoriteShows.js
+++ controllers/shows/favoriteShows.js
@@ -8,6 +8,22 @@ router.get(Api+'/shows/favorite',async(req,res)=>{
     const shows= await db.collection('favorite_shows').find({user_id:req.user._id}).toArray()
     res.send(shows)
 })
+router.delete(Api+'/shows/favorite/:id',async(req,res)=>{
+    try {
+        const { id } = req.params;
+        console.log(id);
+        // Find the show by user id and show id
+        const show = await db.collection('favorite_shows').deleteOne({_id:new ObjectId(id)});
+        console.log(show);
+        if (!show) {
+          return res.status(404).json({ message: 'Show not found' });
+        }
+        return res.json({ message: 'Show deleted successfully' });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+      }
+})
 
 router.post(Api+'/shows/favorite',(req,res)=>{
     const {user_id,...data}=req.body
--- controllers/shows/watchLaterShows.js
+++ controllers/shows/watchLaterShows.js
@@ -8,7 +8,22 @@ router.get(Api+'/shows/watch-later',async(req,res)=>{
     const shows= await db.collection('watch_later_shows').find({user_id:req.user._id}).toArray()
     res.send(shows)
 })
-
+router.delete(Api+'/shows/watch-later/:id',async(req,res)=>{
+    try {
+        const { id } = req.params;
+        console.log(id);
+        // Find the show by user id and show id
+        const show = await db.collection('watch_later_shows').deleteOne({_id:new ObjectId(id)});
+        console.log(show);
+        if (!show) {
+          return res.status(404).json({ message: 'Show not found' });
+        }
+        return res.json({ message: 'Show deleted successfully' });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+      }
+})
 router.post(Api+'/shows/watch-later',(req,res)=>{
     const {user_id,...data}=req.body
     const viewd={...data,user_id:new ObjectId(user_id)}
